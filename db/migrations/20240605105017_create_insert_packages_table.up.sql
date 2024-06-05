51aea00d72a9bf7dec9d2054701b5535732bbf2f
Immanuelrae <immanuel.ekoputro@gmail.com>
2024-06-05 17:53:25 +0700

    feat: add insert migration for packages

---
+++ db/migrations/20240605105017_create_insert_packages_table.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO packages (package_name, package_price, package_duration_days, status) VALUES ('Unlimited Swipe', 100000, 30, 1);
+INSERT INTO packages (package_name, package_price, package_duration_days, status) VALUES ('Verified label for user', 100000, 30, 1);
